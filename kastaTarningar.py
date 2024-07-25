09ef1bdc1030beb5efed21f574c579d8a75778b0
petraelgemyr <petra.elgemyr@gmail.com>
2024-07-25 16:22:58 +0200

    simplify dice throwing

--- kastaTarningar.py
+++ kastaTarningar.py
@@ -5,46 +5,40 @@ Skapa ett program som simulerar kast med två tärningar.
 
 Planering
 1. Skapa en mainfunktion som alltid sker i start av programmet.
-2. Skapa tärningsfunktion för att generera ett slumpmässigt tal mellan 1 till 6 (tärningskast). Använd random: https://www.geeksforgeeks.org/python-randint-function/
-   För dynamisk text för vilken tärning man ska kasta, använd parameter i funktionen och for-loop i mainfunktionen. 
-   Kan enkelt lägga till fler tärningar vid senare tillfälle. 
-3. Tärningsfunktionen ska returnera talet, som ska sparas vid funktionsanropet till en tärningsvariabel beroende på vilket varv i loopen man är på. 
-4. Mainfunktionen ska skriva ut värdet på tärningen.
+2. Användare ska kunna klicka på enter. Detta triggar en funktion som slumpar fram tal mellan 1 till 6 (tärningskast). Använd random: https://www.geeksforgeeks.org/python-randint-function/
+3. Någonstans måste de två talen sparas. Enklast i en lista. Detta måste ske i en funktion som hanterar just tärningskast. Värdena ska returneras. 
+4. Värdena måste tas emot. Sedan måste mainfunktionen skriva ut tärningarnas värden.
+5. Fråga om användaren vill fortsätta kasta. Kastfunktionen anropas inuti en loop.
 
+Brister/Begränsningar
+Användaren kan skriva in något annat än ja/nej när man får frågan om att fortsätta och programmet kommer inte att förstå detta.
+Borde kanske använda en slags keydown funktion eller event som man kan lyssna på istället för en input, dock är det kanske 
+lite överkurs för denna uppgift. 
 
 """
 
 from random import randint
 
 
-# Genererar ett slumpmässigt tal mellan 1 till 6 efter att användaren klickat på enter
-def getRadnomNumber(diceNumber):
-    input(f"Kasta tärning {diceNumber} genom att klicka på enter!")
-    dice = randint(1, 6)
-    return dice
-
-
-# Funktion som fortsätter att kasta tärningarna och skriver ut resultaten
+# Genererar 2 slumpmässiga tal mellan 1 till (till och med) 6 efter att användaren klickat på enter
 def throwDices():
+    dices = []
+    input(f"Kasta tärningarna genom att klicka på enter!")
 
-    firstDice = None
-    secondDice = None
+    for i in range(2):
+        dices.append(randint(1, 6))
 
-    for diceNumber in range(2):
-        if diceNumber == 0:
-            firstDice = getRadnomNumber(diceNumber + 1)
-        else:
-            secondDice = getRadnomNumber(diceNumber + 1)
-
-    print(f"Tärning 1: {firstDice} \nTärning 2: {secondDice}")
+    return dices
 
 
-# Mainfunktion som startat programmet. While-loop som frågar om användaren vill avsluta programmet.
+# Mainfunktion som startar programmet. While-loop som frågar om användaren vill avsluta programmet.
 def main():
     stopExecution = False
 
     while stopExecution == False:
-        throwDices()
+        thrownDices = throwDices()
+        print(f"Tärningarna visar: {thrownDices[0]} och {thrownDices[1]} prickar")
+
         inputCommand = input("Vill du avsluta? (ja/nej): ")
         if inputCommand == "Ja" or inputCommand == "ja" or inputCommand == "JA":
             stopExecution = True
